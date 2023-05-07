da90cf139de9aae0ab79a454c00a69ac91786a00
KnowWisdom <knowisdom314@gmail.com>
2023-05-08 01:33:01 +0900

    ❕ : auth admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.db import models
+from .models import User
+from django.forms import Textarea
 
-# Register your models here.
+class UserAdminConfig(UserAdmin):
+    ordering = ('-created_at',)
+    list_display = ('id', 'email', 'last_login')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'created_at', 'last_login')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active',)}),
+        ('Personal', {'fields': ('open', 'point')}),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            # 'classes': ('wide',),
+            'fields' : ('email', 'password', 'is_active', 'is_staff')}
+        ),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
