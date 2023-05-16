f367125c4b0414164c2f03875b2ba0f60aae371d
KnowWisdom <knowisdom314@gmail.com>
2023-05-16 18:48:51 +0900

    ➕ : 식물 즐겨찾기 관련 기능 완료

--- plant/views.py
+++ plant/views.py
@@ -10,20 +10,43 @@ from .serializer import *
 class RequestListView(APIView):
     # 사용자 요청 모아보기 
     def get(self, request):
-        requests = Request.objects.filter(user = request.user)
+        requests = Request.objects.filter(user_id = request.user)
         serializer = RequestSerializer(requests, many = True)
         return Response(serializer.data, status = status.HTTP_200_OK)
+    
     # 요청 추가하기
     def post(self, request):
-        serializer = RequestSerializer(data=request.data)
+        serializer = RequestCreateSerializer(data=request.data)
         
         if serializer.is_valid():
             data = Request.objects.create(
                 user_id = request.user,
-                content = request.data['context'],
+                content = request.data['content'],
                 reference_photo = request.data['photo']
             )
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
+    
+class BookmarkView(APIView):
+    # 북마크 등록/해제
+    def post(self, request):
+        bookmark = Bookmark.objects.filter(plant_id = request.data['plant_id'], user_id = request.user)
         
+        if bookmark :
+            bookmark[0].delete()
+            message = '북마크 해제'
+        else :
+            Bookmark.objects.create(
+                user_id = request.user,
+                plant_id = request.data['plant_id'],
+            )
+            message = '북마크 등록'
+        return Response({
+            "message" : message
+        }, status = status.HTTP_200_OK)
+
+class BookmarkListView(APIView):
+    def get(self, request):
+        bookmark = Bookmark.objects.filter(user_id = request.user)
+        serializer = BookmarkSerializer(bookmark, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
