c7ec6dbc10fadf1bf0870d3745b3fec404c6d578
KnowWisdom <knowisdom314@gmail.com>
2023-11-15 16:11:59 +0900

    MOD : partner 에러 확인 코드

--- plant/views.py
+++ plant/views.py
@@ -209,6 +209,7 @@ class PartnerView(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status = status.HTTP_201_CREATED)
+            print(serializer.errors)
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
     
